1845977c9f2e5a05547482a4d01f8982435ca5db
Albert Badretdinov <albert_badretdin@mail.ru>
2023-02-08 19:14:49 +0300

    Added PYTHONPATH of LASER source.

--- install_laser.sh
+++ install_laser.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/facebookresearch/LASER.git src/laser
 export LASER=$PWD/src/laser
+export PYTHONPATH="${PYTHONPATH}:${PWD}/src/laser/source"
 bash src/laser/install_models.sh
 bash src/laser/install_external_tools.sh
